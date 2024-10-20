ba877ee046f4b2acb81205fd57f8684b55ee0f54
ofluffydev <fufferpish@gmail.com>
2024-10-19 20:18:10 -0500

    attempt to fix api route

--- src/app/api/images/route.ts
+++ src/app/api/images/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import fs from "fs/promises";
 import path from "path";
 
-export const runtime = "nodejs";
+export const runtime = 'edge';
 
 export async function GET(request: Request) {
   const url = new URL(request.url);
