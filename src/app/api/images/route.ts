b3b1c89d5870a751a214e44592ab396485d30911
ofluffydev <fufferpish@gmail.com>
2024-10-19 20:24:02 -0500

    attempt to fix api route

--- src/app/api/images/route.ts
+++ src/app/api/images/route.ts
@@ -2,8 +2,6 @@ import { NextResponse } from "next/server";
 import fs from "fs/promises";
 import path from "path";
 
-export const runtime = 'edge';
-
 export async function GET(request: Request) {
   const url = new URL(request.url);
   const gallery = url.searchParams.get("gallery") || "kids";
